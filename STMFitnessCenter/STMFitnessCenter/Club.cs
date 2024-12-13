9d4e1f3bdcd8eae6984e71193ef37a51bfee592e
siedlikmichael <hello@grandcircus.co>
2024-12-12 19:35:04 -0500

    created member and club class

---
+++ STMFitnessCenter/STMFitnessCenter/Club.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STMFitnessCenter
+{
+    public class Club
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Club(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
---
+++ STMFitnessCenter/STMFitnessCenter/Member.cs
@@ -0,0 +1,17 @@
+﻿namespace STMFitnessCenter
+{
+    public abstract class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Member(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public abstract void CheckIn(Club club);
+
+
+    }
+}
