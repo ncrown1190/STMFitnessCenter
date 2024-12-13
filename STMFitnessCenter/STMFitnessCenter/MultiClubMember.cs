bb7db985ceef6505aa00d21a5c34c6000bccec1d
Syeda Hussain <syeda.hussain13@gmail.com>
2024-12-12 20:36:02 -0500

    Added SingleClub Class and MultiClub Class

---
+++ STMFitnessCenter/STMFitnessCenter/MultiClubMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STMFitnessCenter
+{
+    public class MultiClubMember : Member
+    {
+        public int MembershipPoints { get; set; }
+        public MultiClubMember(int id, string name): base(id, name) 
+        {
+            MembershipPoints = 0;
+        }
+
+        public override void CheckIn(Club club)
+        {
+            MembershipPoints += 10;
+        }
+    }
+}
---
+++ STMFitnessCenter/STMFitnessCenter/SingleClubMember.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STMFitnessCenter
+{
+    public class SingleClubMember : Member
+    {
+        public Club AssignedClub { get; set; }
+
+        public SingleClubMember(int id, string name, Club club): base(id, name)
+        {
+            AssignedClub = club;
+        }
+
+        public override void CheckIn(Club club)
+        {
+            if (club != AssignedClub)
+            {
+                Console.WriteLine("This is not your club!");
+            } else
+            {
+                Console.WriteLine($"{Name} checked into {club.Name}");
+            }
+
+        }
+    }
+}
